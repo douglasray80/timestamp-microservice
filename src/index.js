871384ceb64f3003b3b158eac03b7fa7a9889ccb
Douglas Ray <dpr980@gmail.com>
2019-04-21 20:37:44 +0100

    added middleware to accept date_string param

--- src/index.js
+++ src/index.js
@@ -11,22 +11,26 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname + 'index.html')
 })
 
-// 3. An empty timestamp should return the current datetime
+// An empty timestamp should return the current datetime
 app.get('/api/timestamp', (req, res) => {
 	const now = new Date()
 	res.json({ unix: now.getTime(), utc: now.toUTCString() })
 })
 
-// 1. Create API endpoint at [project_url]/api/timestamp/:date_string?
+// Create API endpoint at [project_url]/api/timestamp/:date_string?
+app.get('/api/timestamp/:date_string?', (req, res) => {
+	const { date_string } = req.params
+	console.log(date_string)
 
-// 2. Middleware should be able to parse a unix timestamp (integer) and an ISO-8601 format date string
+	// Middleware should be able to parse a unix timestamp (integer) and an ISO-8601 format date string
 
-// 4. A valid date should return JSON having the structure
-// {"unix": <date.getTime()>, "utc" : <date.toUTCString()> }
-// e.g. {"unix": 1479663089000 ,"utc": "Sun, 20 Nov 2016 17:31:29 GMT"}
+	// A valid date should return JSON having the structure
+	// {"unix": <date.getTime()>, "utc" : <date.toUTCString()> }
+	// e.g. {"unix": 1479663089000 ,"utc": "Sun, 20 Nov 2016 17:31:29 GMT"}
 
-// 5. If the date string is invalid the api returns a JSON having the structure
-// {"error" : "Invalid Date" }.
+	// If the date string is invalid the api returns a JSON having the structure
+	// {"error" : "Invalid Date" }.
+})
 
 app.listen(4000, () => {
 	console.log('Your app is listening on http://localhost:4000')
